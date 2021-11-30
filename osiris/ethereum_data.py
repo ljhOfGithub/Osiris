657b794c35e69b3827ef67fed671b67fa9275eea
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2021-11-30 21:32:47 +0800

    update

--- osiris/ethereum_data.py
+++ osiris/ethereum_data.py
@@ -1,5 +1,7 @@
 # this the interface to create your own data source 
 # this class pings etherscan to get the latest code and balance information
+#此接口用于创建自己的数据源
+#这个类ping etherscan以获取最新的代码和平衡信息
 
 import json
 import re
@@ -10,16 +12,16 @@ class EthereumData:
 		self.apiDomain = "https://api.etherscan.io/api"
 		self.apikey = "VT4IW6VK7VES1Q9NYFI74YKH8U7QW9XRHN"
 
-	def getBalance(self, address):
+	def getBalance(self, address):#获取指定地址的合约的余额
 		apiEndPoint = self.apiDomain + "?module=account&action=balance&address=" + address + "&tag=latest&apikey=" + self.apikey
-		r = requests.get(apiEndPoint)
-		result = json.loads(r.text)
+		r = requests.get(apiEndPoint)#发出request请求,返回对象
+		result = json.loads(r.text)#
 		status = result['message']
 		if status == "OK":
-			return result['result'] 
+			return result['result']#判断api请求是否成功
 		return -1
 
-	def getCode(self, address):
+	def getCode(self, address):#获取合约的字节码
 		# apiEndPoint = self.apiDomain + "" + address + "&tag=latest&apikey=" + apikey
 		# no direct endpoint for this
 		r = requests.get("https://etherscan.io/address/" + address + "#code")
--- osiris/ethereum_data1.py
+++ osiris/ethereum_data1.py
@@ -1,15 +1,16 @@
 # this the interface to create your own data source 
 # this class pings a private / public blockchain to get the balance and code information 
-
+#此接口用于创建自己的数据源
+#这个类ping一个私有/公共的区块链来获取余额和代码信息
 from web3 import Web3, KeepAliveRPCProvider
 
 class EthereumData:
 	def __init__(self):
-		self.host = 'x.x.x.x'
+		self.host = 'x.x.x.x'#个人私有链创建的合约余额和字节码
 		self.port = '8545'
 		self.web3 = Web3(KeepAliveRPCProvider(host=self.host, port=self.port))		
 
-	def getBalance(self, address): 
+	def getBalance(self, address):
 		return self.web3.eth.getBalance(address)
 
 	def getCode(self, address):		
